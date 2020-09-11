b4213c1a65344edca90a6b7bf2d7ab8db5c15191
Mateone <54219667+Mateone@users.noreply.github.com>
2020-09-11 19:13:44 -0300

    commit inicial

---
+++ test/Library.Test/PersonTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+using UnitTestAndDebug;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Insertá tu código de inicialización aquí
+        }
+
+        [Test]
+        public void TestPerson1() // Cambiá el nombre para indicar qué estás probando
+        {
+            string expected = 
+        }
+    }
+}
